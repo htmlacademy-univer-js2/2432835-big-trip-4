89292368f86ed92a14b8da9e662709643f45ff11
ShoZaFruct <eva.evas99@mail.ru>
2024-05-25 17:45:36 +0500

    8.8. Дополнительная функциональность fix quotes

--- src/mock/constants.js
+++ src/mock/constants.js
@@ -1,55 +1,55 @@
 const POINT_TYPES = {
-  TAXI: "taxi",
-  BUS: "bus",
-  TRAIN: "train",
-  SHIP: "ship",
-  DRIVE: "drive",
-  FLIGHT: "flight",
-  CHECK_IN: "check-in",
-  SIGHTSEEING: "sightseeing",
-  RESTAURANT: "restaurant",
+  TAXI: 'taxi',
+  BUS: 'bus',
+  TRAIN: 'train',
+  SHIP: 'ship',
+  DRIVE: 'drive',
+  FLIGHT: 'flight',
+  CHECK_IN: 'check-in',
+  SIGHTSEEING: 'sightseeing',
+  RESTAURANT: 'restaurant',
 };
 
-const AUTHORIZATION = "Basic qN3Fsq43cwa4xj3w5";
-const END_POINT = "https://21.objects.htmlacademy.pro/big-trip";
+const AUTHORIZATION = 'Basic qN3Fsq43cwa4xj3w5';
+const END_POINT = 'https://21.objects.htmlacademy.pro/big-trip';
 
 const FilterType = {
-  EVERYTHING: "everything",
-  FUTURE: "future",
-  PAST: "past",
+  EVERYTHING: 'everything',
+  FUTURE: 'future',
+  PAST: 'past',
 };
 
 const SortType = {
-  DAY: "day",
-  EVENT: "event",
-  TIME: "time",
-  PRICE: "price",
-  OFFER: "offer",
+  DAY: 'day',
+  EVENT: 'event',
+  TIME: 'time',
+  PRICE: 'price',
+  OFFER: 'offer',
 };
 
 const UpdateType = {
-  PATCH: "PATCH",
-  MINOR: "MINOR",
-  MAJOR: "MAJOR",
-  INIT: "INIT",
+  PATCH: 'PATCH',
+  MINOR: 'MINOR',
+  MAJOR: 'MAJOR',
+  INIT: 'INIT',
 };
 
 const UserAction = {
-  UPDATE_POINT: "UPDATE_POINT",
-  ADD_POINT: "ADD_POINT",
-  DELETE_POINT: "DELETE_POINT",
+  UPDATE_POINT: 'UPDATE_POINT',
+  ADD_POINT: 'ADD_POINT',
+  DELETE_POINT: 'DELETE_POINT',
 };
 
 const Method = {
-  GET: "GET",
-  PUT: "PUT",
-  POST: "POST",
-  DELETE: "DELETE",
+  GET: 'GET',
+  PUT: 'PUT',
+  POST: 'POST',
+  DELETE: 'DELETE',
 };
 
 const Mode = {
-  DEFAULT: "DEFAULT",
-  EDITING: "EDITING",
+  DEFAULT: 'DEFAULT',
+  EDITING: 'EDITING',
 };
 
 const TimeLimit = {
